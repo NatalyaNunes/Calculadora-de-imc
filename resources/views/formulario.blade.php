75eab4f88a265a20b5ac38ee46485266caa4e57c
Natálya Gabriele Nunes de Azevedo <nunes.natalya@academico.ifrn.edu.br>
2022-12-01 11:50:40 -0300

    atualização com template

--- resources/views/formulario.blade.php
+++ resources/views/formulario.blade.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculadora de imc</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container" >
+@extends('templates.template_principal')
+
+@section('titulo', 'Formulario')
+
+@section('conteudo')
         <h1>Calculadora de IMC</h1>
     <form action="/calcular"method="post">
         {{ csrf_field() }}
@@ -21,9 +15,5 @@
         <input type="text" name="altura" class="form-control" id="altura">
         </div>
         <button type="submit" class="btn btn-primary">Calcular</button>
-    </form>
-    </div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-</body>
-</html>
+       </form>
+@endsection
--- resources/views/resultado.blade.php
+++ resources/views/resultado.blade.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resultado imc</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+@extends('templates.template_principal')
 
-</head>
-<body>
+@section('titulo', 'Resultado')
 
-    <div class="container">
+@section('conteudo')
+
     <h3>O resultado do seu indice é: </h3>  
     <h1>{{ $imc }}</h1> 
     <hr>
@@ -36,9 +29,5 @@
     @endif
 </h1>
     <a class="btn btn-primary" href="/">Voltar</a>
-  
-    </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-</body>
-</html>
+@endsection
